381aaed4f73fc47bdad40f921bb27c750338cbd8
Paula <paulatamiress@outlook.com>
2021-12-12 17:41:30 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -6,20 +8,34 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("Spread Java Developer");
-        curso1.setDescricao("Olá Dev, chegou o momento de você se tornar ainda mais potente para o mercado! " +
-                "\nCom o Bootcamp Spread Java Developer, você aprenderá sobre os principais fundamentos de Java e " +
-                "\nse especializará dentro desta área. Se inscreva agora, aprimore os seus domínios e " +
-                "\ntenha chances de contratação!");
-        curso1.setCargaHoraria(97);
+        curso1.setTitulo("Boas-vindas ao Bootcamp Spread Java Developer");
+        curso1.setDescricao("Conheça a Spread, uma empresa que acredita que boas ideias nascem de bons relacionamentos.");
+        curso1.setCargaHoraria(1);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria #1: Aula inaugural Spread Java Developer");
         mentoria.setDescricao("Aula inaugural tem a função de dar boas vindas aos Devs inscritos no bootcamp.");
         mentoria.setData(LocalDate.of(2021,11,10));
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Spread Java Developer");
+        bootcamp.setDescricao("Olá Dev, chegou o momento de você se tornar ainda mais potente para o mercado! " +
+                "\nCom o Bootcamp Spread Java Developer, você aprenderá sobre os principais fundamentos de Java e se " +
+                "\nespecializará dentro desta área. Se inscreva agora, aprimore os seus domínios " +
+                "\ne tenha chances de contratação!");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPaula = new Dev();
+        devPaula.setNome("Paula");
+        devPaula.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devPaula.getConteudosInscritos());
+        devPaula.progredir();
+        devPaula.progredir();
+        System.out.println("Conteudos concluídos: " + devPaula.getConteudosConcluidos());
+        System.out.println("XP: " + devPaula.calcularTotalXp());
+
+
 
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,40 +1,32 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
+
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
+
         return cargaHoraria;
     }
 
     public void setCargaHoraria(int cargaHoraria) {
+
         this.cargaHoraria = cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso {" +
-                "Título = '" + titulo + '\'' +
-                ", Descrição = '" + descricao + '\'' +
+                "Título = '" + getTitulo() + '\'' +
+                ", Descrição = '" + getDescricao() + '\'' +
                 ", Carga Horária = " + cargaHoraria + " horas" +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+
+        return XP_PADRAO * cargaHoraria;
     }
 }
